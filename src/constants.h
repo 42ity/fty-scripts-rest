902acf154170e464cf58d98ec6c4d87c0816bc61
Jean-Baptiste Boric <Jean-BaptisteBORIC@Eaton.com>
2018-10-10 16:43:06 +0200

    Fix code

--- src/constants.h
+++ src/constants.h
@@ -3,7 +3,7 @@
 
 #include <string>
 
-const std::string SCRIPT_FILENAME_REGEX = "^[a-zA-Z0-9_-][a-zA-Z0-9_.-]{0,254}$";
+const std::string SCRIPT_FILENAME_REGEX = "^[a-zA-Z0-9][a-zA-Z0-9_.-]{0,254}$";
 const std::string SCRIPT_DIRECTORY = "/var/lib/fty/fty-scripts-rest/";
 
 const int SCRIPT_FILE_MAX_SIZE = 64*1024;
--- src/helpers.cc
+++ src/helpers.cc
@@ -2,12 +2,12 @@
 
 void operator>>=(const cxxtools::SerializationInfo &si, std::vector<std::string> &out) {
     if (si.category() != cxxtools::SerializationInfo::Category::Array) {
-        throw std::runtime_error("");
+        throw std::runtime_error("Parameter must be an array");
     }
 
     for (const auto &i : si) {
         if (i.category() != cxxtools::SerializationInfo::Category::Value) {
-            throw std::runtime_error("");
+            throw std::runtime_error("Elements of array must be values");
         }
 
         std::string value;
