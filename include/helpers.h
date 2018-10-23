61b17697ff53b69ad269f15634591b5d7cc45079
Jean-Baptiste Boric <Jean-BaptisteBORIC@Eaton.com>
2018-10-23 14:04:11 +0200

    Remove constants.h file

--- include/helpers.h
+++ include/helpers.h
@@ -8,4 +8,10 @@
 
 void operator>>=(const cxxtools::SerializationInfo &si, std::vector<std::string> &out);
 
+const std::string SCRIPT_FILENAME_REGEX = "^[a-zA-Z0-9][a-zA-Z0-9_.-]{0,254}$";
+const std::string SCRIPT_DIRECTORY = "/var/lib/fty/fty-scripts-rest/";
+
+const int SCRIPT_FILE_MAX_SIZE = 64*1024;
+const int SCRIPT_MAX_FILES = 256;
+
 #endif
