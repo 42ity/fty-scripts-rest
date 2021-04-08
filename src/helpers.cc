44eb9193f943cf005d3e1cdc7279362ca4dbfd80
Degott, Francois Regis <FrancoisRegisDegott@eaton.com>
2021-04-08 15:57:33 +0200

    ecpp
    Signed-off-by: Degott, Francois Regis <FrancoisRegisDegott@eaton.com>

--- src/helpers.cc
+++ src/helpers.cc
@@ -15,6 +15,7 @@
     with this program; if not, write to the Free Software Foundation, Inc.,
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 */
+
 #include "helpers.h"
 
 void operator>>=(const cxxtools::SerializationInfo &si, std::vector<std::string> &out) {
